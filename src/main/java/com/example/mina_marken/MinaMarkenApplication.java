7fc7e2c3fab7e22bb161497dfb2c33bb949b0444
Astrid Rosen <astrid.flisan@gmail.com>
2025-04-13 17:57:51 +0200

    initial commit

---
+++ src/main/java/com/example/mina_marken/MinaMarkenApplication.java
@@ -0,0 +1,13 @@
+package com.example.mina_marken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinaMarkenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinaMarkenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mina_marken/MinaMarkenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mina_marken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinaMarkenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
