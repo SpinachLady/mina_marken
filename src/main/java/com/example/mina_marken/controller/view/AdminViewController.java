fa07d629ff2d992ac4dcaebf584284074d52c34e
Astrid Rosen <astrid.flisan@gmail.com>
2025-04-25 17:06:48 +0200

    add admin start page

--- src/main/java/com/example/mina_marken/controller/view/AdminViewController.java
+++ src/main/java/com/example/mina_marken/controller/view/AdminViewController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminViewController {
 
     @RequestMapping("/start")
-    public String getStartPage(Model model) {
+    public String getStartPage() {
         return "admin/start";
     }
 
